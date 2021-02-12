759513fd8f375a1a5eea877cf5dc06772ac37295
Joao Victor Rosa Sallas <jvsallas@ibm.com>
2021-02-12 01:33:51 -0300

    Initial commit

---
+++ src/test/java/com/mercadosallas/mercadosallas/MercadoSallasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadosallas.mercadosallas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoSallasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
